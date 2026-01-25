b2b6877577c2ffbbf07fd61ad212417286147c02
Adam <adamfeher1995@gmail.com>
2026-01-25 23:30:57 +0100

    Update site content

--- hello.js
+++ hello.js
@@ -1,1 +1,8 @@
 console.log("Hello Worlds");
+
+let button = document.getElementById("myButton");
+let message = document.getElementById("message");
+
+button.addEventListener("click", function() {
+    message.textContent = "Sikeresen rákattintottál a gombra!";
+});
